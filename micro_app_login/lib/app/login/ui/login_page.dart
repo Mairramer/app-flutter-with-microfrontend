533278a98f4d3f8134636686fde8ab708028b126
Mairramer <mairramer.dasilva28@hotmail.com>
2022-01-26 21:56:58 -0300

    tela de login adicionada

--- micro_app_login/lib/app/login/ui/login_page.dart
+++ micro_app_login/lib/app/login/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:micro_app_dependencies/micro_app_dependencies.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,11 +11,99 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    var _height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.orange,
+              child: Center(
+                child: Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: Container()),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                onChanged: (value) {},
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelStyle: const TextStyle(color: Colors.orange),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.orange,
+                          width: 2.0,
+                        )),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: "Seu email"),
+              ),
+            ),
+            SizedBox(
+              height: _height * 0.02,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                onChanged: (value) {},
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelStyle: const TextStyle(color: Colors.orange),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Colors.orange,
+                        width: 2.0,
+                      )),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: "Sua senha",
+                ),
+              ),
+            ),
+            SizedBox(
+              height: _height * 0.06,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Esqueceu sua senha?',
+                style: TextStyle(color: Colors.orange, fontSize: 15),
+              ),
+            ),
+            Observer(
+              builder: (_) {
+                return Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Text('Novo aqui? Crie sua conta')
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
